6dcd7f7900dfd847ead1c7fba0da9a090275b2ea
Молдобаев Эрмек Элмуратович <moldobaev.ermek98@MacBook-Air-Moldobaev.local>
2022-06-07 11:38:39 +0600

    added post request

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ GET - получение данных
 команды для запуска json-server
 json-server -w db.json -p 8000   //!
 */
+
+/* CRUD -Create(POST) Read(GET) Update(PUT & Patch) Delete(Delete)
+ */
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById("inp-add");
+let btnAdd = document.getElementById("btn-add");
+// console.log(inpAdd, btnAdd);
+
+btnAdd.addEventListener("click", function () {
+  let newTodo = {
+    todo: inpAdd.value,
+  };
+  // console.log(newTodo);
+  fetch(API, {
+    method: "POST",
+    body: JSON.stringify(newTodo),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+});
